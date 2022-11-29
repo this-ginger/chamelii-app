43f8c7a9f09dfc48649a9c9791b3596dee4625ff
Douglas <55545456+this-ginger@users.noreply.github.com>
2022-11-29 15:41:35 +0000

    Final

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,7 @@ import os, environ
 
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, True)
+    DEBUG=(bool, False)
 )
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -27,8 +27,8 @@ DEBUG = env('DEBUG')
 ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets') 
 
 # load production server from .env
-ALLOWED_HOSTS        = ['localhost', 'localhost:85', '127.0.0.1',               env('SERVER', default='127.0.0.1') ]
-CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1', 'https://' + env('SERVER', default='127.0.0.1') ]
+ALLOWED_HOSTS        = ['localhost', 'localhost:85', '127.0.0.1', '192.168.68.205', 'chamelii.hillchain.net', '0.0.0.0',               env('SERVER', default='0.0.0.0') ]
+CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1', 'https://' + env('SERVER', default='0.0.0.0') ]
 
 # Application definition
 
