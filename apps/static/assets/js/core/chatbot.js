5e8bef171b4ba3cab4ca6747a1bdcbbffbd06c55
Douglas <55545456+this-ginger@users.noreply.github.com>
2023-02-06 00:01:03 +0000

    Update

--- apps/static/assets/js/core/chatbot.js
+++ apps/static/assets/js/core/chatbot.js
@@ -11,12 +11,33 @@ function chatbotResponse() {
   botMessage = "I'm confused"; //the default message
 
   if (lastUserMessage === 'hi' || lastUserMessage == 'hello') {
-    const hi = ['hi', 'howdy', 'hello']
+    const hi = ['Hi I\'m Chamelii, and this is your safe space. How are you feeling?']
     botMessage = hi[Math.floor(Math.random() * (hi.length))];;
   }
 
+  if (lastUserMessage === 'I am getting angry for no reason and feeling sad') {
+    botMessage = 'Firstly, these emotions are quite normal, we all experience them from time to time. Have you noticed any other changes in your emotions or behaviours?'
+  }
+
+  if (lastUserMessage === 'I don\'t really like seeing people anymore, I\'m worried about angry outbursts') {
+    botMessage === 'That\'s totally understandable and normal. I\'m here to listen in a safe, confidential environment and look for the right type of additional support for you. We can arrange some coaching to help you manage the angry outbursts and will arrange a mental health first aid practitioner to message you directly in your safe space.'
+  }
+
   if (lastUserMessage === 'name' || "what is your name?") {
-    botMessage = 'Hey there! My name is Chamelii, how are you doing today?';
+    botMessage = 'Hi I\'m Chamelii, and this is your safe space. How are you feeling?';
+  }
+
+
+  if (lastUserMessage === 'I\'m so worried about my job, can\'t sleep at night and I feel like I\'m doing everything alone.') {
+    botMessage = 'Firstly it\'s very common to experience these feelings when under pressure, in fact stress can change the way we think, feel and behave, are you seeing any other changes?'
+  }
+
+  if (lastUserMessage === 'I\'m finding it hard to concentrate and want to sleep all day.') {
+    botMessage === 'Well the first step is to consult with a medical professional to determine a diagnosis, it may well be that you\'re experiencing some short-term stress conditions.  We can arrange a confidential appointment for you in your safe space? Or, there are 6 free confidential therapy sessions in your safe space to access. We can also arrange a coach on stress or anxiety to teach you some coping tools when you feel a little overwhelmed.'
+  }
+
+  if (lastUserMessage === 'name' || "what is your name?") {
+    botMessage = 'Hi I\'m Chamelii, and this is your safe space. How are you feeling?';
   }
 
   if (lastUserMessage === 'I\'m unhappy' || lastUserMessage == 'I\'m depressed' || lastUserMessage == 'I\'m distressed' || lastUserMessage == 'I\'m sad' || lastUserMessage == 'I\'m sad' || lastUserMessage == 'I\'m miserable' || lastUserMessage == 'I\'m troubled' || lastUserMessage == 'I\'m low' || lastUserMessage == 'I\'m down' || lastUserMessage == 'I\'m anxious') {
